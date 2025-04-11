2b3d18d59448315d03fd8e42060f48971978216d
ozobi <1027113568@qq.com>
2025-04-11 15:05:35 +0800

    update

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "rspress/config";
 export default defineConfig({
     // 文档根目录
     root: "docs",
+    base: "/Autoxjs_v6_ozobi",
     title: "Autoxjs_v6_ozobi",
     markdown: {
         showLineNumbers: true,
